649c723e0da64eb240bc76bc340298af41438e6b
Michael Bennett <platform-computer-assessment+3@cloud.ionos.com>
2021-05-22 15:58:57 +0000

    fixed unable to access web app remotely. improved error logic

--- app/app.py
+++ app/app.py
@@ -1,15 +1,18 @@
+''' Flask web app '''
+
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from script import cuboid_calculator
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="michaelb",
-    password="ionostest",
-    hostname="85.215.227.215",
-    databasename="cuboidcalc",
-)
+SQLALCHEMY_DATABASE_URI = \
+    "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+        username="michaelb",
+        password="ionostest",
+        hostname="85.215.227.215",
+        databasename="cuboidcalc",
+    )
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -17,6 +20,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 class SavedResults(db.Model):
+    ''' Creates database model '''
 
     __tablename__ = "saved_results"
 
@@ -29,15 +33,32 @@ class SavedResults(db.Model):
     sum_of_edge_lengths = db.Column(db.Float)
 
 
-
-
 saved_volume = ['Empty']
 saved_surface_area = ['Empty']
 saved_sum_of_edge_lengths = ['Empty']
 saved_errors = ['Empty']
 
+
+def input_validator(input_a, input_b, input_c, errors):
+    ''' Check the inputs exist and are positive numbers '''
+    for idx, input_x in enumerate([input_a, input_b, input_c]):
+        if input_x:
+            try:
+                input_x = float(input_x)
+            except ValueError:
+                errors.append(f'{input_x} is not a number.\n')
+            else:
+                if input_x < 1:
+                    errors.append(f'{input_x} is not a positive number.')
+        else:
+            errors.append(f'Field {idx+1} is empty.\n')
+    errors = (" ").join(errors)
+    return errors
+
+
 @app.route("/", methods=["GET", "POST"])
 def main():
+    ''' Main function for serving app and data '''
     errors = []
 
     if request.method == "GET":
@@ -47,28 +68,18 @@ def main():
                                             saved_surface_area=saved_surface_area,
                                             saved_sum_of_edge_lengths=saved_sum_of_edge_lengths,
                                             saved_results=saved_results)
-    
+
     input_a = request.form['input_a']
     input_b = request.form['input_b']
     input_c = request.form['input_c']
-    for idx, input_x in enumerate([input_a, input_b, input_c]):
-        if input_x:
-            try:
-                input_x = float(input_x)
-            except:
-                errors.append(f'{input_x} is not a number.\n')
-            else:
-                if input_x < 1:
-                    errors.append(f'{input_x} is not a positive number.')
-        else:
-            errors.append(f'Field {idx+1} is empty.\n')
-    errors = (" ").join(errors)
+
+    errors = input_validator(input_a, input_b, input_c, errors)
 
     if errors:
         saved_errors.insert(0, errors)
     else:
         if len(saved_errors) > 0:
-            saved_errors.pop()
+            saved_errors[0] = 'Empty'
         result = cuboid_calculator(input_a, input_b, input_c)
         saved_volume.insert(0, result['volume'])
         saved_surface_area.insert(0, result['surface_area'])
@@ -82,10 +93,10 @@ def main():
                                 sum_of_edge_lengths=result['sum_of_edge_lengths'])
         db.session.add(db_entries)
         db.session.commit()
-    
+
 
     return redirect(url_for('main'))
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
